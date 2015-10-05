5b85f15b16f2b8ef58105a525ce739bc9c78851f
Frank Lee <lee.frankie@gmail.com>
2015-10-05 17:05:42 -0400

    update user accounts

--- proxyform.js
+++ proxyform.js
@@ -1,35 +1,61 @@
 if (Meteor.isClient) {
-  Template.section1.onRendered(function() {
-      $('select').material_select()
-  });
+	//Accounts related
+	Template.login.events({
+		'submit form': function(event){
+			event.preventDefault();
+			var email = $('[name=email]').val();
+			var password = $('[name=password]').val();
+
+			Meteor.loginWithPassword(email, password, function(error) {
+				if(error){
+			        console.log(error.reason); // Output error when the login fails
+			    } else {
+			    	Router.go("admin"); // Redirect user when login succeeds	
+			    }
+			});
+		}
+	});
+
+	Template.navigation.events({
+	    'click .logout': function(event){
+	        event.preventDefault();
+	        Meteor.logout(); //logout function
+	        Router.go('page1'); //route back to login
+	    }
+	});	
+
+	//Initialize selection dropdown
+	Template.section1.onRendered(function() {
+		$('select').material_select()
+	});
 
   //not in use currently
-  Template.section2.onRendered(function() {
-    $('.datepicker').pickadate({
+	  Template.section2.onRendered(function() {
+  	$('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
-    });
+	});
   });
 
   //store data in session
   Template.page1.events({
   	'click #nextpage': function(){
-      Session.set('address', $('[name="address"]').val());
-      Session.set('unitnum', $('[name="unitnum"]').val());
-      Session.set('candidate', $('[name="candidate"]').val());
-      Session.set('proxyname', $('[name="proxyname"]').val());
-    }
+  		Session.set('address', $('[name="address"]').val());
+  		Session.set('unitnum', $('[name="unitnum"]').val());
+  		Session.set('candidate', $('[name="candidate"]').val());
+  		Session.set('proxyname', $('[name="proxyname"]').val());
+  	}
   });
 
   //confirmation page submit
   Template.page2.events({
-	'click #submit': function(){
-		var address = Session.get('address');
-		var unitnum = Session.get('unitnum');
-		var candidate = Session.get('candidate');
-		var proxyname = Session.get('proxyname');
-		var owner_name = $('[name="owner_name"]').val();
-		var email = $('[name="email"]').val();
+  	'click #submit': function(){
+  		var address = Session.get('address');
+  		var unitnum = Session.get('unitnum');
+  		var candidate = Session.get('candidate');
+  		var proxyname = Session.get('proxyname');
+  		var owner_name = $('[name="owner_name"]').val();
+  		var email = $('[name="email"]').val();
 
 		//test
 		console.log(address)
@@ -56,22 +82,22 @@ if (Meteor.isClient) {
 		    }
 		});
 	}
-  });
+});
 
   //fields in legal text, will change later
   Template.confirmation.helpers({
-	'address': function(){
-	    return Session.get('address')
-    },
-    'unitnum': function(){
-    	return Session.get('unitnum')
-    },
-    'candidate': function(){
-    	return Session.get('candidate')
-    },
-    'proxyname': function(){
-    	return Session.get('proxyname')
-    }
+  	'address': function(){
+  		return Session.get('address')
+  	},
+  	'unitnum': function(){
+  		return Session.get('unitnum')
+  	},
+  	'candidate': function(){
+  		return Session.get('candidate')
+  	},
+  	'proxyname': function(){
+  		return Session.get('proxyname')
+  	}
   });
 
 }
